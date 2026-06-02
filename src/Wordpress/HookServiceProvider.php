f1828e43229b186e24aec7ca6f44e7e758d5fdfa
Sajeeb Ahamed <sajeeb07ahamed@gmail.com>
2026-06-02 23:56:03 +0600

    Improve the hook service provider

--- src/Wordpress/HookServiceProvider.php
+++ src/Wordpress/HookServiceProvider.php
@@ -7,6 +7,11 @@ use Framework\Wordpress\Hooks\Actions\RegisterRestApi;
 
 class HookServiceProvider extends ServiceProvider
 {
+    protected static $defaults = [
+        'actions' => [RegisterRestApi::class],
+        'filters' => [],
+    ];
+
     /**
      * Register the hooks to the application.
      *
@@ -14,20 +19,40 @@ class HookServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $hooks = $this->hooks();
+
+        $this->app->tag($hooks['actions'], 'hook.actions');
+        $this->app->tag($hooks['filters'], 'hook.filters');
+    }
+
+    /**
+     * Get the hooks from the configuration file.
+     *
+     * @return array
+     * @since 1.0.0
+     */
+    protected function hooks()
+    {
         $hooks_path = $this->app->config_path('hooks.php');
 
         if (!file_exists($hooks_path)) {
-            return;
+            return static::$defaults;
         }
 
         $hooks = include $hooks_path;
+
+        if (empty($hooks)) {
+            return static::$defaults;
+        }
 
         if (!in_array(RegisterRestApi::class, $hooks['actions'], true)) {
             array_unshift($hooks['actions'], RegisterRestApi::class);
         }
 
-        $this->app->tag($hooks['actions'], 'hook.actions');
-        $this->app->tag($hooks['filters'], 'hook.filters');
+        return [
+            'actions' => $hooks['actions'] ?? [],
+            'filters' => $hooks['filters'] ?? [],
+        ];
     }
 
     /**
